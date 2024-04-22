2c7b2d6b15e038cde56b762bdb2a88388361e7f6
sumitrai <developer2607@gmail.com>
2024-04-22 21:15:31 +0530

    task solved

--- app/src/main/java/com/example/dyanamiciconchanger/MainActivity.kt
+++ app/src/main/java/com/example/dyanamiciconchanger/MainActivity.kt
@@ -1,20 +1,64 @@
 package com.example.dyanamiciconchanger
 
+import android.annotation.SuppressLint
+import android.content.ComponentName
+import android.content.pm.PackageManager
+import android.content.pm.ShortcutInfo
+import android.content.pm.ShortcutManager
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Drawable
+import android.graphics.drawable.Icon
+import android.os.Build
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.dyanamiciconchanger.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var shortcutManager: ShortcutManager
+    private lateinit var binding: ActivityMainBinding
+    @RequiresApi(Build.VERSION_CODES.N_MR1)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        shortcutManager = getSystemService(ShortcutManager::class.java)
+
+        // Example: Change app icon to a specific predefined drawable
+        binding.apply {
+            changeIcon.setOnClickListener {
+
+                packageManager?.setComponentEnabledSetting(
+                    ComponentName(applicationContext.packageName, applicationContext.packageName + ".MainActivityAlias1"),
+                    PackageManager.COMPONENT_ENABLED_STATE_ENABLED, PackageManager.DONT_KILL_APP
+                )
+
+                packageManager?.setComponentEnabledSetting(
+                    ComponentName(applicationContext.packageName, applicationContext.packageName + ".MainActivityAlias2"),
+                    PackageManager.COMPONENT_ENABLED_STATE_DISABLED, PackageManager.DONT_KILL_APP
+                )
+
+            }
+
+
+            changeIcon2.setOnClickListener {
+                packageManager?.setComponentEnabledSetting(
+                    ComponentName(applicationContext.packageName, applicationContext.packageName + ".MainActivityAlias1"),
+                    PackageManager.COMPONENT_ENABLED_STATE_DISABLED, PackageManager.DONT_KILL_APP
+                )
+
+                packageManager?.setComponentEnabledSetting(
+                    ComponentName(applicationContext.packageName, applicationContext.packageName + ".MainActivityAlias2"),
+                    PackageManager.COMPONENT_ENABLED_STATE_ENABLED, PackageManager.DONT_KILL_APP
+                )
+            }
         }
+       
     }
+
+
 }
+
