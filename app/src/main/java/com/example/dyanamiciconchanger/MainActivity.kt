3d845a4f7e15242b3fe513a82507232bf1b70531
sumitrai <developer2607@gmail.com>
2024-04-22 22:38:27 +0530

    task solved

--- app/src/main/java/com/example/dyanamiciconchanger/MainActivity.kt
+++ app/src/main/java/com/example/dyanamiciconchanger/MainActivity.kt
@@ -1,64 +1,177 @@
 package com.example.dyanamiciconchanger
 
-import android.annotation.SuppressLint
 import android.content.ComponentName
 import android.content.pm.PackageManager
-import android.content.pm.ShortcutInfo
 import android.content.pm.ShortcutManager
-import android.graphics.Bitmap
-import android.graphics.Canvas
-import android.graphics.drawable.BitmapDrawable
-import android.graphics.drawable.Drawable
-import android.graphics.drawable.Icon
 import android.os.Build
 import android.os.Bundle
+import android.text.Html
+import android.text.Spanned
 import android.widget.Toast
 import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import com.example.dyanamiciconchanger.adapter.ImageSliderAdapter
 import com.example.dyanamiciconchanger.databinding.ActivityMainBinding
+import java.util.Timer
+import java.util.TimerTask
 
 class MainActivity : AppCompatActivity() {
     private lateinit var shortcutManager: ShortcutManager
     private lateinit var binding: ActivityMainBinding
+
+    private lateinit var imageSliderAdapter: ImageSliderAdapter
+    private val images = listOf(R.drawable.p1, R.drawable.p2, R.drawable.p3)
+
+
     @RequiresApi(Build.VERSION_CODES.N_MR1)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         shortcutManager = getSystemService(ShortcutManager::class.java)
-
         // Example: Change app icon to a specific predefined drawable
+        imageSliderAdapter = ImageSliderAdapter(this, images)
         binding.apply {
-            changeIcon.setOnClickListener {
-
-                packageManager?.setComponentEnabledSetting(
-                    ComponentName(applicationContext.packageName, applicationContext.packageName + ".MainActivityAlias1"),
-                    PackageManager.COMPONENT_ENABLED_STATE_ENABLED, PackageManager.DONT_KILL_APP
-                )
-
-                packageManager?.setComponentEnabledSetting(
-                    ComponentName(applicationContext.packageName, applicationContext.packageName + ".MainActivityAlias2"),
-                    PackageManager.COMPONENT_ENABLED_STATE_DISABLED, PackageManager.DONT_KILL_APP
-                )
-
-            }
 
 
-            changeIcon2.setOnClickListener {
-                packageManager?.setComponentEnabledSetting(
-                    ComponentName(applicationContext.packageName, applicationContext.packageName + ".MainActivityAlias1"),
-                    PackageManager.COMPONENT_ENABLED_STATE_DISABLED, PackageManager.DONT_KILL_APP
-                )
+            viewPager.adapter = imageSliderAdapter
 
-                packageManager?.setComponentEnabledSetting(
-                    ComponentName(applicationContext.packageName, applicationContext.packageName + ".MainActivityAlias2"),
-                    PackageManager.COMPONENT_ENABLED_STATE_ENABLED, PackageManager.DONT_KILL_APP
-                )
-            }
+            val timer = Timer()
+            timer.scheduleAtFixedRate(SliderTimerTask(), 3000, 3000)
+
+
+
+
         }
-       
+
+//        clickAction()
     }
 
 
+
+//    private fun clickAction() {
+//        binding.apply {
+//            changeIcon.setOnClickListener {
+//                packageManager?.setComponentEnabledSetting(
+//                    ComponentName(
+//                        applicationContext.packageName,
+//                        applicationContext.packageName + ".MainActivityAlias1"
+//                    ),
+//                    PackageManager.COMPONENT_ENABLED_STATE_ENABLED, PackageManager.DONT_KILL_APP
+//                )
+//
+//                packageManager?.setComponentEnabledSetting(
+//                    ComponentName(
+//                        applicationContext.packageName,
+//                        applicationContext.packageName + ".MainActivityAlias2"
+//                    ),
+//                    PackageManager.COMPONENT_ENABLED_STATE_DISABLED, PackageManager.DONT_KILL_APP
+//                )
+//
+//            }
+//
+//
+//            changeIcon2.setOnClickListener {
+//                packageManager?.setComponentEnabledSetting(
+//                    ComponentName(
+//                        applicationContext.packageName,
+//                        applicationContext.packageName + ".MainActivityAlias1"
+//                    ),
+//                    PackageManager.COMPONENT_ENABLED_STATE_DISABLED, PackageManager.DONT_KILL_APP
+//                )
+//
+//                packageManager?.setComponentEnabledSetting(
+//                    ComponentName(
+//                        applicationContext.packageName,
+//                        applicationContext.packageName + ".MainActivityAlias2"
+//                    ),
+//                    PackageManager.COMPONENT_ENABLED_STATE_ENABLED, PackageManager.DONT_KILL_APP
+//                )
+//            }
+//            infoIcon.setOnClickListener {
+//                showPremiumDialog()
+//            }
+//        }
+//
+//    }
+
+    private fun showPremiumDialog() {
+        // Create an AlertDialog.Builder instance
+        val builder = AlertDialog.Builder(this)
+
+        // Set dialog title
+        builder.setTitle("Special Premium Offer")
+
+        // Create a bulleted list of premium features
+        val features = listOf(
+            "Ad-Free Experience",
+            "Low Price Guarantee",
+            "Customizable App Icons"
+        )
+
+        // Format the features into a bulleted list
+        val formattedFeatures = formatBulletList(features)
+
+        // Set dialog message with formatted bullet points
+        builder.setMessage(formattedFeatures)
+
+        // Set positive button (OK)
+        builder.setPositiveButton("Claim Offer") { dialog, which ->
+            // Handle claim offer action here (e.g., navigate to upgrade screen)
+            // For demonstration, we'll just show a toast message
+         //   showToast(context, "Redirecting to claim offer...")
+            packageManager?.setComponentEnabledSetting(
+                ComponentName(
+                    applicationContext.packageName,
+                    applicationContext.packageName + ".MainActivityAlias1"
+                ),
+                PackageManager.COMPONENT_ENABLED_STATE_DISABLED, PackageManager.DONT_KILL_APP
+            )
+
+            packageManager?.setComponentEnabledSetting(
+                ComponentName(
+                    applicationContext.packageName,
+                    applicationContext.packageName + ".MainActivityAlias2"
+                ),
+                PackageManager.COMPONENT_ENABLED_STATE_ENABLED, PackageManager.DONT_KILL_APP
+            )
+            Toast.makeText(this, "App icon changed successfully ", Toast.LENGTH_SHORT).show()
+            dialog.dismiss()
+        }
+
+        // Set negative button (Cancel)
+        builder.setNegativeButton("Maybe Later") { dialog, which ->
+            dialog.dismiss()
+        }
+
+        // Create and show the AlertDialog
+        val dialog = builder.create()
+        dialog.show()
+    }
+
+    // Function to format a list of items into a bulleted list
+    fun formatBulletList(items: List<String>): Spanned {
+        val stringBuilder = StringBuilder()
+        stringBuilder.append("<ul>")
+        for (item in items) {
+            stringBuilder.append("<li>")
+            stringBuilder.append(item)
+            stringBuilder.append("</li>")
+        }
+        stringBuilder.append("</ul>")
+        return Html.fromHtml(stringBuilder.toString())
+    }
+
+
+    inner class SliderTimerTask : TimerTask() {
+        override fun run() {
+            runOnUiThread {
+               binding.viewPager.currentItem = (binding.viewPager.currentItem + 1) % images.size
+            }
+        }
+    }
+
 }
+
 
---
+++ app/src/main/java/com/example/dyanamiciconchanger/adapter/ImageSliderAdapter.kt
@@ -0,0 +1,31 @@
+package com.example.dyanamiciconchanger.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.dyanamiciconchanger.R
+
+class ImageSliderAdapter(private val context: Context, private val images: List<Int>) :
+    RecyclerView.Adapter<ImageSliderAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.item_image_slider, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val imageRes = images[position]
+        holder.imageView.setImageResource(imageRes)
+    }
+
+    override fun getItemCount(): Int {
+        return images.size
+    }
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val imageView: ImageView = itemView.findViewById(R.id.imageView)
+    }
+}
